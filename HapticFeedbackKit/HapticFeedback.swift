1a11d8ef7d8dbdc4bf7272de30cc9b80e77293cc
Zane6w <zhwin@outlook.com>
2021-04-06 12:45:26 +0800

    Conversion to Swift 5

--- HapticFeedbackKit/HapticFeedback.swift
+++ HapticFeedbackKit/HapticFeedback.swift
@@ -8,51 +8,19 @@
 
 import UIKit
 import AVFoundation
-import AudioToolbox
-
-
-fileprivate var audioPlayer: AVAudioPlayer?
-
-/// 震动反馈示例音乐
-fileprivate func playSound(forResource: String) {
-    let bundle = Bundle(identifier: "com.zhi.HapticFeedbackKit")
-    let path = bundle?.path(forResource: "FeedbackSounds", ofType: "bundle")
-    let fileBundle = Bundle(path: path!)
-    
-    let url = fileBundle?.url(forResource: forResource, withExtension: "mp4")
-    
-    audioPlayer = try? AVAudioPlayer(contentsOf: url!)
-    audioPlayer?.play()
-}
 
 /// 触觉反馈 （适用于 iPhone 7、7 Plus 及其以上机型）
-open class HapticFeedback {
+public struct HapticFeedback {
     
     @available(iOS 10.0, *)
     public enum Notification {
         
-        fileprivate static var generator: UINotificationFeedbackGenerator = {
+        private static var generator: UINotificationFeedbackGenerator = {
             let generator = UINotificationFeedbackGenerator()
             generator.prepare()
             
             return generator
         }()
-        
-        public static func function() {
-            print(generator)
-        }
-        
-        public static func successSound() {
-            playSound(forResource: "success")
-        }
-        
-        public static func warningSound() {
-            playSound(forResource: "warning")
-        }
-        
-        public static func errorSound() {
-            playSound(forResource: "error")
-        }
         
         
         public static func success() {
@@ -67,30 +35,16 @@ open class HapticFeedback {
             occurred(.error)
         }
         
-        fileprivate static func occurred(_ notificationType: UINotificationFeedbackType) {
+        private static func occurred(_ notificationType: UINotificationFeedbackGenerator.FeedbackType) {
             generator.notificationOccurred(notificationType)
             generator.prepare()
         }
-        
     }
     
     @available(iOS 10.0, *)
     public enum Impact {
         
-        fileprivate static var generator: UIImpactFeedbackGenerator?
-        
-        
-        public static func lightSound() {
-            playSound(forResource: "impact_light")
-        }
-        
-        public static func mediumSound() {
-            playSound(forResource: "impact_medium")
-        }
-        
-        public static func heavySound() {
-            playSound(forResource: "impact_heavy")
-        }
+        private static var generator: UIImpactFeedbackGenerator?
         
         
         public static func light() {
@@ -105,53 +59,46 @@ open class HapticFeedback {
             impactOccurred(.heavy)
         }
         
-        fileprivate static func impactOccurred(_ style: UIImpactFeedbackStyle) {
+        private static func impactOccurred(_ style: UIImpactFeedbackGenerator.FeedbackStyle) {
             generator = UIImpactFeedbackGenerator(style: style)
             generator?.prepare()
             generator?.impactOccurred()
         }
-        
     }
     
     @available(iOS 10.0, *)
     public enum Selection {
         
-        fileprivate static var generator: UISelectionFeedbackGenerator = {
+        private static var generator: UISelectionFeedbackGenerator = {
             let generator = UISelectionFeedbackGenerator()
             generator.prepare()
             
             return generator
         }()
-        
-        
-        public static func selectionSound() {
-            playSound(forResource: "selection")
-        }
-        
+
         
         public static func selection() {
             generator.selectionChanged()
             generator.prepare()
         }
-        
     }
     
     @available(iOS 9.0, *)
-    open class func peek() {
+    public static func peek() {
         AudioServicesPlaySystemSound(1519)
     }
     
     @available(iOS 9.0, *)
-    open class func pop() {
+    public static func pop() {
         AudioServicesPlaySystemSound(1520)
     }
     
     @available(iOS 9.0, *)
-    open class func error() {
+    public static func error() {
         AudioServicesPlaySystemSound(1521)
     }
     
-    open class func vibration() {
+    public static func vibration() {
         AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
     }
     
--- TestFeedback/ViewController.swift
+++ TestFeedback/ViewController.swift
@@ -13,39 +13,32 @@ class ViewController: UIViewController {
     
     
     @IBAction func playNotificationSuccessSound(_ sender: UIButton) {
-        HapticFeedback.Notification.successSound()
         HapticFeedback.Notification.success()
     }
     
     @IBAction func playNotificationWarningSound(_ sender: UIButton) {
-        HapticFeedback.Notification.warningSound()
         HapticFeedback.Notification.warning()
     }
     
     @IBAction func playNotificationErrorSound(_ sender: UIButton) {
-        HapticFeedback.Notification.errorSound()
         HapticFeedback.Notification.error()
     }
     
     
     @IBAction func playImpactLightSound(_ sender: UIButton) {
-        HapticFeedback.Impact.lightSound()
         HapticFeedback.Impact.light()
     }
     
     @IBAction func playImpactMediumSound(_ sender: UIButton) {
-        HapticFeedback.Impact.mediumSound()
         HapticFeedback.Impact.medium()
     }
     
     @IBAction func playImpactHeavySound(_ sender: UIButton) {
-        HapticFeedback.Impact.heavySound()
         HapticFeedback.Impact.heavy()
     }
     
     
     @IBAction func playSelectionSound(_ sender: UIButton) {
-        HapticFeedback.Selection.selectionSound()
         HapticFeedback.Selection.selection()
     }
     
